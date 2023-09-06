dc3cb9d61c8d754324215294f4d2dabee65304d6
janice <janice4350341@gmail.com>
2023-09-06 11:04:58 +0800

    commit

--- taipei-day-trip/taipei-day-trip/app.py
+++ taipei-day-trip/taipei-day-trip/app.py
@@ -24,7 +24,7 @@ def thankyou():
 #API取得景點資料列表----------------------------------------------
 @app.route("/api/attractions", methods=["GET"])
 def apiattractions():
-    page = int(request.args.get("page", 0))  
+    page = int(request.args.get("page", 0))
     per_page = 12  # 每頁顯示的資料筆數
     keyword = request.args.get("keyword")
     
@@ -40,52 +40,28 @@ def apiattractions():
         cursor = con.cursor()
         
         if keyword:
-            # 執行SQL指令，去資料庫裏面查詢
-            query = "SELECT id, name, category, description, address, transport, mrt, lat, lng, images FROM attractions WHERE mrt = %s OR name LIKE %s"
+            # 使用 SQL 的 LIMIT 和 OFFSET 實現分頁
+            query = "SELECT id, name, category, description, address, transport, mrt, lat, lng, images FROM attractions WHERE mrt = %s OR name LIKE %s LIMIT %s OFFSET %s"
             keyword_pattern = f"%{keyword}%"
-            cursor.execute(query, (keyword, keyword_pattern))
-            
-            # 將查詢到的資料存放在taipeidaytrip的變數中
-            taipeidaytrip = cursor.fetchall()
+            cursor.execute(query, (keyword, keyword_pattern, per_page, page * per_page))
         else:
             # 如果未提供關鍵字，則檢索所有資料
-            query = "SELECT id, name, category, description, address, transport, mrt, lat, lng, images FROM attractions"
-            cursor.execute(query)
+            query = "SELECT id, name, category, description, address, transport, mrt, lat, lng, images FROM attractions LIMIT %s OFFSET %s"
+            cursor.execute(query, (per_page, page * per_page))
             
-            taipeidaytrip = cursor.fetchall()
+        # 將查詢到的資料存放在taipeidaytrip的變數中
+        taipeidaytrip = cursor.fetchall()
 
         # 關閉游標和連線
         cursor.close()
         con.close()
-        
-        total_records = len(taipeidaytrip) # 計算資料總長度
-        total_pages = (total_records // per_page) # 計算頁數
 
-        if total_records % per_page > 0:
-            total_pages += 1
-
-        start_idx = page  * per_page
-        end_idx = start_idx + per_page
-        paginated_data = taipeidaytrip[start_idx:end_idx]
-
-        # 檢查是否為最後一頁
-        is_last_page = page == total_pages - 1
-
-        # 檢查是否有足夠的數據
-        enough_data = total_records >= per_page  # 使用 >= 來檢查是否有足夠的數據
-
-        # 設定 next_page 的值
-        if is_last_page and not enough_data:  # 只有在是最後一頁且不足 12 筆資料時設定 next_page 為 None
-            next_page = None
-        else:
-            next_page = page + 1
-
-        if total_records - (page * per_page) < per_page:
-            next_page = None
+        # 檢查是否還有更多的資料
+        has_more = len(taipeidaytrip) == per_page
 
         if taipeidaytrip:
             attractions = []
-            for row in paginated_data:  # 使用分頁後的資料
+            for row in taipeidaytrip:
                 # 將圖片URL轉換為Python列表
                 images = json.loads(row[9])
 
@@ -106,6 +82,8 @@ def apiattractions():
                 }
                 attractions.append(attraction)
 
+            next_page = page + 1 if has_more else None
+
             response_data = {
                 "nextPage": next_page,
                 "data": attractions
@@ -115,7 +93,7 @@ def apiattractions():
             return json.dumps({"error": True}, ensure_ascii=False), 404
     except mysql.connector.Error as err:
         print("Error connecting to the database:", err)
-        return json.dumps({"error": True,"message": "伺服器內部錯誤"}, ensure_ascii=False), 500
+        return json.dumps({"error": True, "message": "伺服器內部錯誤"}, ensure_ascii=False), 500
 
 
 
@@ -138,44 +116,44 @@ def get_attraction(attractionID):
             cursor.execute("SELECT id, name, category, description, address, transport, mrt, lat, lng, images FROM attractions WHERE id = %s", (attractionID,))
             
             # 將查詢到的資料存放在taipeidaytrip的變數中
-            taipeidaytrip_attractionid = cursor.fetchall()
+            taipeidaytrip_attractionid = cursor.fetchone()  # 使用fetchone()获取一行数据而不是fetchall()
+
         # 關閉游標和連線
         cursor.close()
         con.close()      
 
         if taipeidaytrip_attractionid:
             print(taipeidaytrip_attractionid)
-            attractions = []
-            for row in taipeidaytrip_attractionid: 
-                # 將圖片URL轉換為Python列表
-                images = json.loads(row[9])
 
-                # 移除圖片URL中的反斜杠符號
-                cleaned_images = [image.replace("\\", "") for image in images]
+            # 將圖片URL轉換為Python列表
+            images = json.loads(taipeidaytrip_attractionid[9])
 
-                attraction = {
-                    "id": row[0],
-                    "name": row[1],
-                    "category": row[2],
-                    "description": row[3],
-                    "address": row[4],
-                    "transport": row[5],
-                    "mrt": row[6],
-                    "lat": float(row[7]),
-                    "lng": float(row[8]),
-                    "images": cleaned_images  # 使用處理後的圖片URL列表
-                }
-                attractions.append(attraction)
+            # 移除圖片URL中的反斜杠符號
+            cleaned_images = [image.replace("\\", "") for image in images]
+
+            
+            attraction = {
+                "id": taipeidaytrip_attractionid[0],
+                "name": taipeidaytrip_attractionid[1],
+                "category": taipeidaytrip_attractionid[2],
+                "description": taipeidaytrip_attractionid[3],
+                "address": taipeidaytrip_attractionid[4],
+                "transport": taipeidaytrip_attractionid[5],
+                "mrt": taipeidaytrip_attractionid[6],
+                "lat": float(taipeidaytrip_attractionid[7]),
+                "lng": float(taipeidaytrip_attractionid[8]),
+                "images": cleaned_images  # 使用處理後的圖片URL列表
+            }
 
             response_data = {
-                "data": attractions
+                "data": attraction  # 直接返回单个景点字典
             }
             return json.dumps(response_data, ensure_ascii=False), 200
         else:
-            return json.dumps({"error": True,"message":"景點編號不正確"}, ensure_ascii=False), 404
+            return json.dumps({"error": True, "message": "景點編號不正確"}, ensure_ascii=False), 404
     except mysql.connector.Error as err:
         print("Error connecting to the database:", err)
-        return json.dumps({"error": True,"message": "伺服器內部錯誤"}, ensure_ascii=False), 500
+        return json.dumps({"error": True, "message": "伺服器內部錯誤"}, ensure_ascii=False), 500
 
 
 #API取得捷運站名稱列表--------------------------------------------------
